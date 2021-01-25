05fd06f62e21d0ebf63e75edf82b94396d41e11a
phantie <funtom1605@gmail.com>
2021-01-26 01:00:56 +0200

    optimized branching

--- yself/__init__.py
+++ yself/__init__.py
@@ -2,52 +2,42 @@ from functools import wraps
 from types import FunctionType
 
 __all__ = ('yield_self',)
-__version__ = '1.0'
+__version__ = '1.1'
 
 class specialstaticmethod:
-    def __init__(self, f):
-        self.f = f
+    def __init__(self, f): self.f = f
 
     def __get__(self, obj, cls=None):
         def newfunc(*args, **kwargs):
-            return self.f(
-                (type(obj) if cls is None else cls) if obj is None else obj, 
-                *args, 
-                **kwargs)
+            return self.f(cls if obj is None else obj, *args, **kwargs)
         return newfunc
 
 class specialclassmethod:
-    def __init__(self, f):
-        self.f = f
+    def __init__(self, f): self.f = f
 
     def __get__(self, obj, cls=None):
         def newfunc(*args, **kwargs):
-            return self.f(
-                cls, 
-                (type(obj) if cls is None else cls) if obj is None else obj, 
-                *args, 
-                **kwargs)
-
+            return self.f(cls,  cls if obj is None else obj, *args, **kwargs)
         return newfunc
 
 def yield_self(f):
     if isinstance(f, FunctionType):
         @wraps(f)
-        def wrap(self, *args, **kwargs):
-            f(self, *args, **kwargs)
-            return self
+        def wrap(called_on, *args, **kwargs):
+            f(called_on, *args, **kwargs)
+            return called_on
         return wrap
     elif isinstance(f, classmethod):
         @wraps(f.__func__)
-        def wrap(cls, self, /, *args, **kwargs):
+        def wrap(cls, called_on, /, *args, **kwargs):
             f.__func__(cls, *args, **kwargs)
-            return self
+            return called_on
         return specialclassmethod(wrap)
     elif isinstance(f, staticmethod):
         @wraps(f.__func__)
-        def wrap(cls_or_inst, /, *args, **kwargs):
+        def wrap(called_on, /, *args, **kwargs):
             f.__func__(*args, **kwargs)
-            return cls_or_inst
+            return called_on
         return specialstaticmethod(wrap)
     else:
         raise RuntimeError('unknown argument type', type(f))
